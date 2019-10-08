91e5c9bf414deede476ed54fed7b9b72c1b61c53
Danny Ockilson <d.wilson@waracle.com>
2019-10-08 17:50:17 +0100

    output data

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+import * as data from './data.json';
+
 import { render } from './renderer';
+
+console.log(data.results);
 
 const things = ['My Neighbors the Yamadas', 'Grave of the Fireflies'];
 render(`
